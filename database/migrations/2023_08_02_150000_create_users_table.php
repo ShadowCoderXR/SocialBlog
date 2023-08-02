5db67243878422bd9ea1971fab7afbcb94bdda58
Alexia Natali <alexia_1320114063@uptecamac.edu.mx>
2023-08-02 10:54:26 -0600

    Feat: Users

--- database/migrations/2023_08_02_150000_create_users_table.php
+++ database/migrations/2023_08_02_150000_create_users_table.php
@@ -13,11 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string("slug")->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->unsignedBigInteger('role_id')->nullable();
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('roles')
+                ->onDelete('set null');
+            $table->string('image')->nullable();
+            $table->string('name')->nullable();
+            $table->text('description')->nullable();
+            $table->string('career')->nullable();
+            $table->boolean('status')->default(true);
             $table->timestamps();
         });
     }
