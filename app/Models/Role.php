cc5dfeed538d187c0988552c2b653eda7fb259c2
YaelBalbuenaMartinez <yael_1321124280@uptecamac.edu.mx>
2023-08-02 10:42:48 -0600

    Feat: Roles fue agregado

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     use HasFactory;
+
+    protected $table='roles';
+
+    protected $fillable=[
+        'id',
+        'name'
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(User::class, 'role_id');
+    }
+
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +19,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $roles = ['admin','user'];
+        foreach ($roles as $role) {
+            Role::create([
+                'name' => $role,
+            ]);
+        }
+
     }
 }
