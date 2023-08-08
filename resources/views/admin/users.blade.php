f15bed60e62494cd5b1fc3405bab4e9b0782faef
YaelBalbuenaMartinez <yael_1321124280@uptecamac.edu.mx>
2023-08-08 05:03:54 -0600

    Fix: views admin

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -1,67 +1,161 @@
 @extends('layouts.app')
 
-@section('content')
-    <table>
-        <thead>
-            <tr>
-                <th>Fecha</th>
-                <th>Nombre</th>
-                <th>Email</th>
-                <th>Imagen</th>
-                <th>Descripcion</th>
-                <th>Rol</th>
-                <th>Estado</th>
-                <th>ver perfil</th>
-                <th>Desactivar</th>
-                <th>Eliminar</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($users as $user)
-                <tr>
-                    <td>
-                        {{$user->created_at}}
-                    </td>
-                    <td>
-                        {{$user->name}}
-                    </td>
-                    <td>
-                        {{$user->email}}
-                    </td>
-                    <td>
-                        <img src="{{ asset('storage/images/profiles/' . $user->image) }}" class="img-publicacion" alt="..." width="100">
-                    </td>
-                    <td>
-                        {{$user->description}}
-                    </td>
-                    <td>
-                        {{$user->role->name}}
-                    </td>
-                    <td>
-                        @if($user->status == 1)
-                            Activo
-                        @else
-                            Inactivo
-                        @endif
-                    </td>
-                    <td>
-                        <a href="{{route('profile.show', ['slug' => $user->slug])}}">Ver perfil</a>
-                    <td>
-                        <form action="{{ route('admin.disable.user', ['id' => $user->id]) }}" method="post">
-                            @csrf
-                            @method('PUT')
-                            <button type="submit">Desactivar</button>
-                        </form>
-                    </td>
-                    <td>
-                        <form action="{{ route('admin.destroy.user', ['id' => $user->id]) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit">Eliminar</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-@endsection
+@vite(['resources/css/styles.css', 'resources/js/datatables-simples-demo.js', 'resources/js/scripts.js'])
+
+<nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+    <!-- Navbar Brand-->
+    <a class="navbar-brand ps-3" href="index.html">Social Blog</a>
+    <!-- Sidebar Toggle-->
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+    <!-- Navbar Search-->
+
+    <!-- Navbar-->
+    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="#!">Settings</a></li>
+                <li><a class="dropdown-item" href="#!">Activity Log</a></li>
+                <li>
+                    <hr class="dropdown-divider" />
+                </li>
+                <li><a class="dropdown-item" href="#!">Logout</a></li>
+            </ul>
+        </li>
+    </ul>
+</nav>
+
+<title>Dashboard - SB Admin</title>
+<link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+<link href="css/styles.css" rel="stylesheet" />
+<script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Publicaciones</div>
+                        <a class="nav-link" href="posts">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Admin publicaciones
+                        </a>
+                        <div class="sb-sidenav-menu-heading">Usuarios</div>
+                        <a class="nav-link" href="users">
+                            <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
+                            Admin usuarios
+                        </a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Admin usuarios</h1>
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-table me-1"></i>
+                            DataTable 
+                        </div>
+                        <div class="card-body">
+                            <table id="datatablesSimple">
+                                <thead>
+                                    <tr>
+                                        <th>Fecha</th>
+                                        <th>Nombre</th>
+                                        <th>Email</th>
+                                        <th>Imagen</th>
+                                        <th>Descripcion</th>
+                                        <th>Rol</th>
+                                        <th>Estado</th>
+                                        <th>ver perfil</th>
+                                        <th>Desactivar</th>
+                                        <th>Eliminar</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($users as $user)
+                                    <tr>
+                                        <td>
+                                            {{$user->created_at}}
+                                        </td>
+                                        <td>
+                                            {{$user->name}}
+                                        </td>
+                                        <td>
+                                            {{$user->email}}
+                                        </td>
+                                        <td>
+                                            @if($user->image != null)
+                                            <div class="rounded-circle-wrapper">
+                                                <img src="{{ asset('storage/images/profiles/' . $user->image) }}" alt="avatar" class="img-fluid rounded-circle-image" width="75">
+                                            </div>
+                                            @else
+                                            <div class="rounded-circle bg-secondary text-light d-flex justify-content-center align-items-center" style="width: 75px; height: 75px;">
+                                                <span>Sin imagen</span>
+                                            </div>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            {{$user->description}}
+                                        </td>
+                                        <td>
+                                            {{$user->role->name}}
+                                        </td>
+                                        <td>
+                                            @if($user->status == 1)
+                                            <span class="badge bg-success">Activo</span>
+                                            @else
+                                            <span class="badge bg-danger">Inactivo</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <a href="{{route('profile.show', ['slug' => $user->slug])}}" class="btn btn-primary">Ver perfil</a>
+                                        <td>
+                                            <form action="{{ route('admin.disable.user', ['id' => $user->id]) }}" method="post">
+                                                @csrf
+                                                @method('PUT')
+                                                <button type="submit" class="btn btn-warning">Desactivar</button>
+                                            </form>
+                                        </td>
+                                        <td>
+                                            <form action="{{ route('admin.destroy.user', ['id' => $user->id]) }}" method="post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Eliminar</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2023</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
+</body>
